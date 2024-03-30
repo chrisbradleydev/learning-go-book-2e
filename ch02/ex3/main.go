5f4a7335450d6459b071eb4b57a8658984befd47
Chris Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2024-03-30 13:31:05 -0400

    ch02 exercises

---
+++ ch02/ex3/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var b byte = 255
+	var smallI int32 = math.MaxInt32
+	var bigI uint64 = math.MaxUint64
+
+	b += 1
+	smallI += 1
+	bigI += 1
+
+	fmt.Println(b)
+	fmt.Println(smallI)
+	fmt.Println(bigI)
+}
